bf0a7cb1eb2cfb0f1224b754bcc16987f9594bd9
Mirza Cengic <mirzaceng@gmail.com>
2018-02-12 16:35:19 +0100

    Added milkun17 path

--- R/milkunize.R
+++ R/milkunize.R
@@ -9,19 +9,21 @@
 #' @examples milkunize("Projects/Land_use")
 milkunize <- function(path, drive = "m1")
 {
-  stopifnot(drive %in% c("m1", "m2", "m5"))
+  stopifnot(drive %in% c("m1", "m2", "m5", "m17"))
 
   if(Sys.info()["sysname"] == "Windows"){
   basepath_milkun1 <- "\\\\milkun1-srv.science.ru.nl/milkun1/Mirza_Cengic/"
   # .basepath_LU <- "\\\\milkun1-srv.science.ru.nl/Mirza_Cengic/Projects/Land_use/"
   basepath_milkun2 <- "\\\\milkun2-srv.science.ru.nl/milkun2/Mirza_Cengic/"
   basepath_milkun5 <- "\\\\milkun5-srv.science.ru.nl/milkun5/Merit/"
+  basepath_milkun17 <- "\\\\milkun17-srv.science.ru.nl/milkun17/"
 
 } else {
   basepath_milkun1 <- "/vol/milkun1/Mirza_Cengic/"
   basepath_milkun2 <- "/vol/milkun2/Mirza_Cengic/"
   # .basepath_LU <- "/vol/milkun1/Mirza_Cengic/Projects/Land_use/"
   basepath_milkun5 <- "/vol/milkun5/Merit/"
+  basepath_milkun17 <- "/vol/milkun17/"
 }
 
   if (drive == "m1")
@@ -35,6 +37,10 @@ milkunize <- function(path, drive = "m1")
   if (drive == "m5")
   {
     mypath <- paste0(basepath_milkun5, path)
+  }
+  if (drive == "m17")
+  {
+    mypath <- paste0(basepath_milkun17, path)
   }
   return(mypath)
 }
