3d2bf280124860d79c71a0da5d1f4f56b431408f
Mirza Cengic <m.cengic@science.ru.nl>
2018-08-09 14:59:08 +0200

    Added function to convert from raster object to ggplot-compatible object

---
+++ R/raster_to_gg.R
@@ -0,0 +1,22 @@
+#' Raster* to ggplot object
+#' Convert raster* object to dataframe suitable for making maps with ggplot2.
+#'
+#' @param x Raster* object.
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' library(raster)
+#' library(Rahat)
+#'
+#' raster_to_gg(raster(volcano))
+raster_to_gg <- function(x)
+{
+  stopifnot(inherits(x, "Raster"))
+
+  x <- as(x, "SpatialPixelsDataFrame")
+  x <- as.data.frame(x)
+  names(x) <- c("value", "x", "y")
+  return(x)
+}
